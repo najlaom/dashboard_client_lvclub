a60a726fb9136b8223ab88033f7d1622cee4b35b
najlaom <najla.omrani@esprit.tn>
2021-12-01 15:02:28 +0100

    commit2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: SidebarComponent, children: [
+      {
+        path: 'dashboard', component: DashboardComponent
+      },
+    ]
+    ///Ecommerce
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
